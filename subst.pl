c726c6d79a98d233b7e902123dff5020bd38bbec
Joachim Breitner <mail@joachim-breitner.de>
2020-09-29 11:38:39 +0200

    Make regrex more liberal (fixes #6)

--- subst.pl
+++ subst.pl
@@ -2,13 +2,13 @@
 use File::Slurp;
 
 while (<>) {
-    s{LISTING\(([a-zA-Z/.-]*)\)}{
+    s{LISTING\(([^)]*)\)}{
       "```haskell\n" . read_file("$1") . "```\n"
     }gxe;
-    s{EDIT\(([a-zA-Z/.-]*)\)}{
+    s{EDIT\(([^)]*)\)}{
       "https://code.world/haskell#" . (read_file("$1.hash") =~ s/\n$//r)
     }gxe;
-    s{RUN\(([a-zA-Z/.-]*)\)}{
+    s{RUN\(([^)]*)\)}{
       '<iframe width="400" height="400" src="https://code.world/run.html?mode=haskell&amp;dhash=' . (read_file("$1.dhash") =~ s/\n$//r) . '"></iframe>'
     }gxe;
     print;
